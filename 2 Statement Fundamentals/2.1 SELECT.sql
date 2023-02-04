16033b20b79d873be369fb6b70ad2778eba9402f
Matheus <78040711+matheusriale@users.noreply.github.com>
2023-02-04 01:42:57 -0300

    Delete 2 Statement Fundamentals directory

--- 2 Statement Fundamentals/2.1 SELECT.sql
+++
@@ -1,5 +0,0 @@
--- Use a SELECT statement to grab the first
--- and last names of every customer and
--- their email address
-
-SELECT first_name,last_name,email FROM customer
--- 2 Statement Fundamentals/2.2 SELECT DISTINCT.sql
+++
@@ -1,6 +0,0 @@
--- Retrieve the distinct rating types our films could 
--- have in our database
-
-SELECT DISTINCT rating FROM film;
-
-
